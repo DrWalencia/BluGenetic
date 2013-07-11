7f928e91535b5e39fc8e817fa1d4081da0a8ec91
Pablo Valencia González <valeng.pablo@gmail.com>
2013-07-11 18:11:43 +0200

    Basic structure of a Perl module

--- README.md
+++
@@ -1,4 +0,0 @@
-BluGenetic
-==========
-
-Library aimed to solve problems by using Genetic Algoritms. Focused on simplicity and clearness.
---
+++ lib/BluGenetic.pm
@@ -0,0 +1,111 @@
+package BluGenetic;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+BluGenetic - The great new BluGenetic!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use BluGenetic;
+
+    my $foo = BluGenetic->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Pablo Valencia Gonzalez, C<< <hybrid-rollert at lavabit.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-blugenetic at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=BluGenetic>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc BluGenetic
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=BluGenetic>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/BluGenetic>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/BluGenetic>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/BluGenetic/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Pablo Valencia Gonzalez.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of BluGenetic
